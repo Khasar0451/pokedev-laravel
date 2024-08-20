7479708ccf525c2a9d46774f07bdd25e5824dd66
Executed419 <lucker10@interia.pl>
2024-08-20 11:49:32 +0200

    added logo and layout file

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.cdnfonts.com/css/pokemon-solid" rel="stylesheet">
+    <title>Document</title>
+    <style>
+        h1 {
+            color:yellow;               
+            -webkit-text-stroke-width: 1px;
+            -webkit-text-stroke-color: rgb(43, 101, 209);
+            font-family: 'Pokemon Solid', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Pokédex</h1>
+    {{ $slot }}
+</body>
+
+</html>
--- resources/views/pokemon/list.blade.php
+++ resources/views/pokemon/list.blade.php
@@ -1,13 +1,16 @@
-    <form method="GET" action="/search">
-        <input type="text" name="search" value="{{ request('search') }}">
-        <button type="submit">Search</button>
-    </form>
+    <x-layout>
+        <form method="GET" action="/search">
+            <input type="text" name="search" value="{{ request('search') }}">
+            <button type="submit">Search</button>
+        </form>
 
-    <h2><a href="favouritesIndex">These are your ✨favourite✨ pokémon</a></h2>
+        <h2><a href="favouritesIndex">These are your ✨favourite✨ pokémon</a></h2>
 
-    @foreach ($pokemons as $pokemon)
-        <a href="{{ $pokemon['name'] }}">
-            <li>{{ $pokemon['name'] }} - <a href="{{ $pokemon['name'] }}/addtofavourites">Add to favourites</a> / <a href="{{ $pokemon['name'] }}/removefromfavourites">Remove from favourites</a> <br> </li> 
-        </a>
-    @endforeach
-    {{ $pokemons->links() }}
+        @foreach ($pokemons as $pokemon)
+            <a href="{{ $pokemon['name'] }}">
+                <li>{{ $pokemon['name'] }} - <a href="{{ $pokemon['name'] }}/addtofavourites">Add to favourites</a> / <a
+                        href="{{ $pokemon['name'] }}/removefromfavourites">Remove from favourites</a> <br> </li>
+            </a>
+        @endforeach
+        {{ $pokemons->links() }}
+    </x-layout>
