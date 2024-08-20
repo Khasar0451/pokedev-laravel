71fe18e5f98f70cbac2808f05b373e0f112551ed
Executed419 <lucker10@interia.pl>
2024-08-20 09:28:19 +0200

    searchbar now works

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -7,14 +7,18 @@ use Illuminate\Support\Facades\Http;
 use App\Models\Pokemon;
 class PokemonController extends Controller
 {
-    
-    public function index(){
-        // $response = Http::get('http://pokeapi.co/api/v2/pokemon')->json('results');
-        $pokemons = Pokemon::simplePaginate(10);
-        return view ('pokemon.list', [
+    public function showList($pokemons)
+    {
+        return view('pokemon.list', [
             'pokemons' => $pokemons
         ]);
     }
+    
+    public function index(){
+        $pokemons = Pokemon::simplePaginate(10);
+        return $this->showList($pokemons);
+    }
+
 
     public function show($name){
         $response = Http::get("http://pokeapi.co/api/v2/pokemon/{$name}")->json();
@@ -27,13 +31,8 @@ class PokemonController extends Controller
     public function unfav($name) {}
     
     public function search(){
-        $response = Http::get('http://pokeapi.co/api/v2/pokemon');
-
-        dd(collect($response->json()['results'])->pluck('name')->all());
         $search = request()->input('search');
-        $names = collect($response->json()['results'])->pluck('name')->all();
-        dd($names);
-        $pokemons = collect($names)->where('', 'like', "%$search%")->get(0);
-        // dd( $pokemons );
+        $pokemons = Pokemon::where('name','like','%'.$search.'%')->simplePaginate(10);
+        return $this->showList($pokemons);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PokemonController;
 
 Route::get('/', [PokemonController::class, 'index']);
+Route::get('/search', [PokemonController::class, 'search']);
 Route::get('/{pokemon}', [PokemonController::class, 'show']);
 Route::get('/{pokemon}/fav', [PokemonController::class, 'fav']);
 Route::get('/{pokemon}/unfav', [PokemonController::class, 'unfav']);
-Route::get('/search', [PokemonController::class, 'search']);
